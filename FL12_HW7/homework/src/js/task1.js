09c1b88a8475c9a7a84863a27ff0bc0a65473682
igorogerchuk <igor.ogerchuk@gmail.com>
2020-01-10 18:00:38 +0200

    to merge

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,6 +1,6 @@
 let email = prompt('Enter your email, please');
 
 if (email === null) {
-
+                                                                                                                                                              
 }
 console.log(email);
