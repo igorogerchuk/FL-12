eb4f360fb4b0fa135ca2c6a704e87ad8b90a2e6b
Igor <igor.ogerchuk@gmail.com>
2020-01-28 23:41:15 +0200

    for submition

--- FL12_HW12/homework/src/app.js
+++ FL12_HW12/homework/src/app.js
@@ -1,4 +1,24 @@
+const root = document.querySelector('#root');
+let title = document.createElement('h1');
+root.appendChild(title);
+const addButton = document.createElement('button');
+addButton.textContent = 'Add set';
+addButton.addEventListener('click', onAddHandler);
+function onAddHandler() {
+    location.hash = '/add';
+}
+title.textContent = 'Main page';
+root.appendChild(addButton);
 
+window.addEventListener('hashchange', onHashChangeHandler);
 
-// Your code goes here
+function onHashChangeHandler() {
+    if (location.hash.substring(1) === '/') {
+        title.textContent = 'Main page';
+        root.appendChild(addButton);
+    }
 
+    if (location.hash.substring(1) === '/add') {
+        title.textContent = 'Add new set';
+    }
+}
