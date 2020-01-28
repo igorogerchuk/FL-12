4080c4eca373d2185dd78c689fc2ad4f485dda07
Igor <igor.ogerchuk@gmail.com>
2020-01-29 00:23:34 +0200

    added routes

--- FL12_HW12/homework/src/app.js
+++ FL12_HW12/homework/src/app.js
@@ -1,24 +1,55 @@
 const root = document.querySelector('#root');
 let title = document.createElement('h1');
 root.appendChild(title);
+
+//Main page wrapper
+const mainDiv = document.createElement('div');
+mainDiv.classList.add('hidden');
 const addButton = document.createElement('button');
 addButton.textContent = 'Add set';
 addButton.addEventListener('click', onAddHandler);
 function onAddHandler() {
     location.hash = '/add';
 }
-title.textContent = 'Main page';
-root.appendChild(addButton);
+mainDiv.appendChild(addButton);
 
-window.addEventListener('hashchange', onHashChangeHandler);
+//Add new set wrapper
+const addDiv = document.createElement('div');
+addDiv.classList.add('hidden');
 
-function onHashChangeHandler() {
+//Modify set wrapper
+const modifyDiv = document.createElement('div');
+modifyDiv.classList.add('hidden');
+
+root.appendChild(mainDiv);
+root.appendChild(addDiv);
+root.appendChild(modifyDiv);
+
+
+
+routesHandler();
+
+window.addEventListener('hashchange', routesHandler);
+
+function routesHandler() {
     if (location.hash.substring(1) === '/') {
         title.textContent = 'Main page';
-        root.appendChild(addButton);
+        mainDiv.classList.remove('hidden');
+        addDiv.classList.add('hidden');
+        modifyDiv.classList.add('hidden');
     }
 
     if (location.hash.substring(1) === '/add') {
         title.textContent = 'Add new set';
+        mainDiv.classList.add('hidden');
+        addDiv.classList.remove('hidden');
+        modifyDiv.classList.add('hidden');
+    }
+
+    if (location.hash.substring(1) === '/modify/:item_id') {
+        title.textContent = 'Modify set';
+        mainDiv.classList.add('hidden');
+        addDiv.classList.add('hidden');
+        modifyDiv.classList.remove('hidden');
     }
 }
